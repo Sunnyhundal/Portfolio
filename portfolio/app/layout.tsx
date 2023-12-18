ba758914a75a2faa37f521a796c3b7fbed11e784
Sunny Hundal <amandip.h@gmail.com>
2023-12-17 23:06:21 -0800

    Initial commit

---
+++ portfolio/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sunny | Portfolio",
+  description:
+    "Sunny is a a full-stack developer with a passion for making accessible websites that just work.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 h-[5000px]`}
+      >
+        <div className="bg-[blue] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] opacity-20 sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[red] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem] opacity-20  sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
