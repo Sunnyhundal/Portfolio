1e07e6b6ee0ac1e30edf1335878efb3161dd7b98
Sunny Hundal <amandip.h@gmail.com>
2023-12-21 23:50:43 -0800

    Basic layout for about me  section

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/about";
 import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center pt-32">
+    <main className="flex flex-col items-center flex-wrap justify-center pt-32">
       <Intro />
+      <About />
     </main>
   );
 }
