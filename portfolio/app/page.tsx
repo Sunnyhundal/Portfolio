d16d85ed86dc43edf8b573d77de563678ca8d7c5
Sunny Hundal <amandip.h@gmail.com>
2023-12-28 16:18:10 -0800

    Update layout and components

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,13 +1,15 @@
 import About from "@/components/about";
+import Contact from "@/components/contact";
 import Intro from "@/components/intro";
-import Project from "@/components/project";
+import Projects from "@/components/project";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center pt-32">
       <Intro />
       <About />
-      <Project />
+      <Projects />
+      <Contact />
     </main>
   );
 }
--- portfolio/components/header.tsx
+++ portfolio/components/header.tsx
@@ -9,35 +9,38 @@ export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header className="flex fixed z-[999] p-3 md:p-6 bg-slate-200 justify-between items-center opacity-80 drop-shadow-md w-full">
-      <div className="text-2xl md:text-3xl font-semibold">Sunny Hundal</div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <header className="flex fixed z-[999] p-3 md:p-6 bg-slate-200 justify-between items-center opacity-80 drop-shadow-md w-full">
+        <div className="text-2xl md:text-3xl font-semibold">Sunny Hundal</div>
 
-      <nav className={`md:flex ${showMenu ? "block" : "hidden"}`}>
-        <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
-          {links.map((link) => (
-            <li key={link.hash} className="text-stone-500">
-              <Link
-                href={link.hash}
-                className="hover:text-gray-900"
-              >
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+        <nav className={`md:flex ${showMenu ? "block" : "hidden"}`}>
+          <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+            {links.map((link) => (
+              <li key={link.hash} className="text-stone-500">
+                <Link href={link.hash} className="hover:text-gray-900">
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-      <div className="md:hidden">
-        <button
-          className="p-2"
-          onClick={() => setShowMenu(!showMenu)}
-          aria-label="Toggle Menu"
-        >
-          <div className="w-6 h-px bg-gray-900 my-1"></div>
-          <div className="w-6 h-px bg-gray-900 my-1"></div>
-          <div className="w-6 h-px bg-gray-900 my-1"></div>
-        </button>
-      </div>
-    </header>
+        <div className="md:hidden">
+          <button
+            className="p-2"
+            onClick={() => setShowMenu(!showMenu)}
+            aria-label="Toggle Menu"
+          >
+            <div className="w-6 h-px bg-gray-900 my-1"></div>
+            <div className="w-6 h-px bg-gray-900 my-1"></div>
+            <div className="w-6 h-px bg-gray-900 my-1"></div>
+          </button>
+        </div>
+      </header>
+    </motion.div>
   );
 }
