78b94dde6276be483030fc3315b80048c7d24a74
Sunny Hundal <amandip.h@gmail.com>
2023-12-28 16:12:59 -0800

    Create transition for Know Me block

--- portfolio/components/about.tsx
+++ portfolio/components/about.tsx
@@ -1,27 +1,41 @@
-import React from "react";
+"use client";
+
+import React, { use } from "react";
 import Image from "next/image";
 import { skills } from "@/lib/data";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
-    <div className="flex flex-col lg:flex-row justify-center lg:justify-between mt-5 lg:w-2/3 lg:h-2/3">
-      <div className="bg-slate-200 p-6 lg:p-24 grow opacity-75 lg:w-1/3 lg:mr-2.5 rounded-xl drop-shadow-md mb-5 lg:mb-0">
-        <p className="text-xl font-semibold">Get to know me</p>
-        <p className="text-l">
-          I'm a full-stack developer located on Vancouver Island, BC. I'm
-          passionate about making accessible websites that just work. I'm
-          currently working on a few projects, including a website for a local
-          business and a personal project that I'm hoping to launch soon. if
-          you're interested in working together, please reach out to me. I'd
-          love to hear from you!
-        </p>
-        <div className="flex justify-center lg:justify-end mt-5 lg:mt-10">
-          <button className="flex rounded-xl bg-purple-500 p-2">
-            {" "}
-            Contact me{" "}
-          </button>
+    <div
+      className="flex flex-col lg:flex-row justify-center lg:justify-between mt-5 lg:w-2/3 lg:h-2/3"
+      id="about"
+    >
+      <motion.div
+        className="bg-slate-200 p-6 lg:p-24 grow opacity-75 lg:w-1/3 lg:mr-2.5 rounded-xl drop-shadow-md mb-5 lg:mb-0 whitespace-pre-wrap"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.25 }}
+      >
+        <div>
+          <p className="text-xl font-semibold">Get to know me</p>
+          <p className="text-l">
+            I'm a full-stack developer located on Vancouver Island, BC. I'm
+            passionate about making accessible websites that just work. I'm
+            currently working on a few projects, including a website for a local
+            business and a personal project that I'm hoping to launch soon. if
+            you're interested in working together, please reach out to me. I'd
+            love to hear from you!
+          </p>
+          <div className="flex justify-center lg:justify-end mt-5 lg:mt-10">
+            <button className="flex rounded-xl bg-purple-500 p-2 drop-shadow-md">
+              {" "}
+              Contact me{" "}
+            </button>
+          </div>
         </div>
-      </div>
+      </motion.div>
+
       <div className="bg-white p-6 lg:p-24 grow rounded-xl lg:ml-2.5 drop-shadow-md">
         <p className="text-xl font-semibold">Skills</p>
         <div className="grid grid-cols-1 gap-2 md:grid-cols-1">
