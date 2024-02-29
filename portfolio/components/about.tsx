f145b992e907a068f87029e7f07f4d1f1b399f5c
Sunnyhundal <amandip.h@gmail.com>
2024-02-28 22:28:17 -0800

    Update about component styles

--- portfolio/components/about.tsx
+++ portfolio/components/about.tsx
@@ -8,11 +8,11 @@ import { motion } from "framer-motion";
 export default function About() {
   return (
     <div
-      className="flex flex-col lg:flex-row justify-center lg:justify-between mt-5 lg:w-2/3 lg:h-2/3"
+      className="flex flex-col lg:flex-row justify-center lg:justify-between mt-5 lg:w-2/3 lg:h-2/3 md:items-center lg:items-stretch"
       id="about"
     >
       <motion.div
-        className="bg-slate-200 p-6 lg:p-12 grow opacity-75 lg:w-1/3 lg:mr-2.5 rounded-xl drop-shadow-md mb-5 lg:mb-0 whitespace-pre-wrap"
+        className="bg-slate-200 p-6 lg:p-12 grow opacity-75 lg:w-1/3 lg:mr-2.5 rounded-xl drop-shadow-md mb-5 lg:mb-0 whitespace-pre-wrap md:w-2/3"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.25 }}
@@ -27,22 +27,22 @@ export default function About() {
             you're interested in working together, please reach out to me. I'd
             love to hear from you!
           </p>
-          <div className="flex flex-col justify-center lg:justify-end mt-5 lg:mt-10">
+          <div className="flex justify-center lg:justify-end mt-5 lg:mt-10">
             <button className="mt-4 bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded">
               <a href="#contact">Contact me</a>
             </button>
           </div>
-
         </div>
       </motion.div>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
+        className="flex items-center lg:h-2/3 mb-5 lg:mb-0 md:w-2/3 md:items-center lg:items-stretch"
       >
-        <div className="bg-white p-6 lg:p-12 grow rounded-xl lg:ml-2.5 drop-shadow-md">
+        <div className="bg-white p-6 lg:p-12 grow rounded-xl lg:ml-2.5 drop-shadow-md md:w-2/3">
           <p className="text-xl font-semibold mb-5">Skills</p>
-          <div className="grid grid-cols-1 gap-1 md:grid-cols-3 lg:grid-cols-1">
+          <div className="grid grid-cols-1 gap-1 md:grid-cols-3 lg:grid-cols-1 md:gap-10">
             <SkillList title="Front-end" area="Frontend" />
             <SkillList title="Back-end" area="Backend" />
             <SkillList title="Other" area="Tools" />
@@ -56,7 +56,7 @@ export default function About() {
 function SkillList({ title, area }: { title: string; area: string }) {
   return (
     <div>
-      <p className="text-l">{title}</p>
+      <p className="text-l font-bold pb-4">{title}</p>
       <ul className="flex flex-wrap">
         {skills
           .filter((skill) => skill.area === area)
