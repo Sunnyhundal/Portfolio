6448007f71344cbcfd8ff63bf28b1b3ce06e52b6
Sunnyhundal <amandip.h@gmail.com>
2024-04-16 13:21:52 -0700

    Refactor intro component content formatting in portfolio

--- portfolio/components/intro.tsx
+++ portfolio/components/intro.tsx
@@ -24,23 +24,14 @@ export default function Intro() {
             /> */}
           </div>
           <p className="text-base md:text-xl">
-            <span className="flex flex-col text-2xl md:text-4xl font-bold mb-3">
-              Hi, I'm Sunny.
-            </span>{" "}
-            I'm a <span className="font-semibold">full-stack developer</span>{" "}
-            with a passion for making{" "}
-            <span className="font-semibold">accessible websites </span> that
-            just work. I come from a background in{" "}
-            <span className="font-semibold">
-              economics and international education
-            </span>
-            , and am always looking for ways to combine my interests in
-            technology and education to{" "}
-            <span className="font-semibold">
-              {" "}
-              make the internet a better place.
-            </span>
-          </p>
+  <span className="flex flex-col text-2xl md:text-4xl font-bold mb-3">
+    Hi, I&apos;m Sunny.
+  </span>{" "}
+  I&apos;m a <span className="font-semibold">full-stack developer</span> with a passion for making{" "}
+  <span className="font-semibold">accessible websites</span> that just work. I come from a background in{" "}
+  <span className="font-semibold">economics and international education</span>, and am always looking for ways to combine my interests in technology and education to{" "}
+  <span className="font-semibold">make the internet a better place.</span>
+</p>
         </div>
       </div>
     </motion.div>
